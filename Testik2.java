3b686992cbf26d493223eedca97190d8b21dae7d
Olha Nosal <olgetta1221@gmail.com>
2023-05-07 19:39:03 +0300

    description

--- Testik2.java
+++ Testik2.java
@@ -2,5 +2,7 @@ public class Testik2 {
     public static void main(String[] args) {
         System.out.println("love");
         System.out.println("in love");
+        System.out.println("description");
+
     }
 }
