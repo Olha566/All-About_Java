402cadaf4a6c9f123ecadba431c32b2a79f1f886
Olha566 <122012871+Olha566@users.noreply.github.com>
2023-06-09 12:50:53 +0300

    Update Roadmap.java

--- Testik2.java
+++ Testik2.java
@@ -1,8 +1,15 @@
 public class Testik2 {
     public static void main(String[] args) {
-        System.out.println("love");
-        System.out.println("in love");
-        System.out.println("description");
+       1. Collections (List(ArrayList, LinkedList и Vector), Set(HashSet, TreeSet и LinkedHashSet), Map(HashMap, TreeMap, LinkedHashMap и Hashtable), 
+                       Queue(LinkedList, PriorityQueue и ArrayDeque), Stack(Stack и ArrayDeque), Deque(ArrayDeque и LinkedList). 
+                       Java 8 и выше: Stream и Optional.
+                       Класс Collections(методы такие как сортировка, поиск максимального/минимального элемента и другие операции)
+                       
+       2. Exceptions
+       
+       3. Generics
+       
+       4. Intro to Threads (start(); run(); sleep(); Runnable; Thread)
 
     }
 }
