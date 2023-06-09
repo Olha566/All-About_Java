558ebd1eb289b038f817702b51cadae25bf003b1
Olha566 <122012871+Olha566@users.noreply.github.com>
2023-06-09 12:55:47 +0300

    Update and rename Testik2.java to Roadmap.java

--- Roadmap.java
+++ Roadmap.java
@@ -1,15 +1,17 @@
-public class Testik2 {
+public class Roadmap {
     public static void main(String[] args) {
-       1. Collections (List(ArrayList, LinkedList и Vector), Set(HashSet, TreeSet и LinkedHashSet), Map(HashMap, TreeMap, LinkedHashMap и Hashtable), 
+       1. Recursion
+        
+       2. Collections (List(ArrayList, LinkedList и Vector), Set(HashSet, TreeSet и LinkedHashSet), Map(HashMap, TreeMap, LinkedHashMap и Hashtable), 
                        Queue(LinkedList, PriorityQueue и ArrayDeque), Stack(Stack и ArrayDeque), Deque(ArrayDeque и LinkedList). 
                        Java 8 и выше: Stream и Optional.
                        Класс Collections(методы такие как сортировка, поиск максимального/минимального элемента и другие операции)
                        
-       2. Exceptions
+       3. Exceptions
        
-       3. Generics
+       4. Generics
        
-       4. Intro to Threads (start(); run(); sleep(); Runnable; Thread)
+       5. Intro to Threads (start(); run(); sleep(); Runnable; Thread)
 
     }
 }
